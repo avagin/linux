eef284247f939fa2540a7ba08d68777b21b03c58
Andrei Vagin <avagin@openvz.org>
2017-12-01 23:06:02 -0800

    don't checkout a brach if it has not been changed

--- scripts/update.py
+++ scripts/update.py
@@ -29,16 +29,16 @@ run_cmd("git push -f origin master")
 for t in trees:
     branch="%s-%s" % (t[0], t[1])
     run_cmd("git fetch %s" % t[0])
-    run_cmd("git branch -D %s || true" % branch)
-    run_cmd("git checkout -f %s/%s -b %s" % (t[0], t[1], branch))
-    run_cmd("git cherry-pick origin/master")
-    p = subprocess.Popen("git diff --stat origin/%s" % branch, stdout = subprocess.PIPE, shell = True)
+    p = subprocess.Popen("git diff --stat origin/%s~1 %s/%s" % (branch, t[0], t[1]), stdout = subprocess.PIPE, shell = True)
     out = p.stdout.read()
     p.stdout.close()
     ret = p.wait()
     print out
     if ret == 0 and not out:
         continue
+    run_cmd("git branch -D %s || true" % branch)
+    run_cmd("git checkout -f %s/%s -b %s" % (t[0], t[1], branch))
+    run_cmd("git cherry-pick origin/master")
     run_cmd("git push -f origin %s" % branch)
 
 run_cmd("git checkout master")
